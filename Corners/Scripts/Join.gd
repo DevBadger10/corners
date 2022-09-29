173cbd2ebcd16dee0b199a8c8abaeeb23e8102d6
DevBadger10 <104037142+DevBadger10@users.noreply.github.com>
2022-09-29 10:32:17 +1000

    Add files via upload

--- Corners/Scripts/Join.gd
+++ Corners/Scripts/Join.gd
@@ -3,11 +3,8 @@ extends Node2D
 var PortOldText = "7500"
 
 func _on_Port_text_changed():
-	if $Port.text.length() > 4:
+	if $Port.text.length() > 4 or not $Port.text.is_valid_integer(): # I was setting it before it was used with the old value before. Then I realised I could package it all together!
 		$Port.text = PortOldText
 		$Port.cursor_set_column(4)
 	else:
 		PortOldText = $Port.text
-	if not str(int($Port.text)).length() == $Port.text.length() or not int($Port.text) == 0:
-		$Port.text = PortOldText
-		$Port.cursor_set_column(4)
